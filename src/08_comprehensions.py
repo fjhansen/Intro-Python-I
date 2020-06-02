e1e5fae8ee046340486af7eee1e242a3cd660b07
frances-hansen <francesjuniperhansen@users.noreply.github.com>
2020-06-01 21:57:42 -0700

    commit

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,10 +10,18 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
+##y = []
+##x = range(6)
+##
+##for n in x:
+##    y.append(++n)
+
 y = []
 
-print (y)
 
+for n in range(6):
+    y.append(++n)
+    print(y)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
